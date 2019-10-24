f9ee18632f06d6f8ffd3138fa9ca3502a1991189
Arun M <arunsubmail@gmail.com>
2019-10-24 10:35:33 +0530

    converting nested folders with js files into another extension

--- loop.js
+++ loop.js
@@ -1,38 +1,39 @@
 const fs = require('fs');
+const path = require('path');
 
-function readDirectory() {
-    return new Promise((resolve, reject) => {
-        fs.readdir(__dirname, (err, files) => {
-            if (err) {
-                reject('retrieve failed-' + "/t" + err);
-            }
-            let checkedFiles = checkCorrectFiles(files);
-            resolve(checkedFiles);
-        })
-    });
-}
-
-function checkCorrectFiles(files) {
-    let tempArray = [];
-    for (const file of files) {
-        const regexFile = file.match(/.+\.js$/g);
-        if (regexFile !== null) {
-            let fileStr = regexFile[0];
-            tempArray.push(fileStr);
-        }
+async function readSyncSubDir(dirPath, folderList = []) {
+    const arrayList = fs.readdirSync(dirPath);
+    let directories = [], fileList = [];
+    arrayList.forEach(file => {
+        if (isDirectory(dirPath, file))
+            directories = [...directories, file];
+        else
+            fileList = [...fileList, file];
+    })
+    if (directories.length) {
+        directories.forEach(folder => {
+            folderList = [...folderList, path.join(dirPath, folder)];
+            readSyncSubDir(path.join(dirPath, folder), folderList);
+        });
     }
-    console.log(tempArray);
-    return tempArray;
+    if (fileList.length) {
+        const createdDir = await makeDestDir(dirPath);
+        await createFiles(createdDir, dirPath, fileList, 'jsx').then((res) => {
+            console.log('res', res);
+        });
+    }
 }
+
+const isDirectory = (dirName, file) => fs.statSync(path.join(dirName, file)).isDirectory();
 
 function changeExtension(file, extName) {
     let ext = extName ? '.' + extName : '.js';
-    return file.replace(/\.js$/g, ext);
+    return file.substr(-3) === ".js" ? file.replace(/\.js$/g, ext) : file;
 }
 
-async function createFiles(dirName, files, extName) {
-    for (const file of files) {
-        let reading = await readFiles(file);
+async function createFiles(dirName, readPath, fileList, extName) {
+    for (const file of fileList) {
+        let reading = await readFiles(readPath, file);
         let fileExt = changeExtension(file, extName);
         fs.writeFile(`${dirName}/${fileExt}`, reading, (err) => {
             if (err) {
@@ -43,59 +44,33 @@ async function createFiles(dirName, files, extName) {
     }
 }
 
-function readFiles(regexFile) {
+function readFiles(readPath, regexFile) {
+    const newPath = path.join(readPath, regexFile);
     return new Promise((resolve, reject) => {
-        fs.readFile(regexFile, (err, data) => {
+        fs.readFile(newPath, (err, data) => {
             if (err) {
                 reject(err);
             }
-            resolve(data.toString());
+            resolve(String(data));
         })
     })
 }
 
-function getRandomArbitrary() {
-    let randomDir = __dirname.slice() + (Math.random() * (10 - 1) + 1).toFixed(2);
+function getRandomArbitrary(dirPath) {
+    let randomDir = dirPath.slice() + (Math.random() * (10 - 1) + 1).toFixed(2);
     return randomDir;
 }
 
-function makeDestDir() {
+function makeDestDir(dirPath) {
     return new Promise((resolve, reject) => {
-        console.log('mkdir')
-        const randomDir = getRandomArbitrary();
-        fs.mkdir(randomDir, (err, data) => {
+        const randomDir = getRandomArbitrary(dirPath);
+        fs.mkdir(randomDir, (err) => {
             if (err) {
                 reject('create directory failed because ' + err);
             }
-            console.log(randomDir + ' created')
             resolve(randomDir);
         })
     })
 }
 
-// function writeToDist(reading) {
-//     fs.writeFile
-// }
-
-async function loopDirectory() {
-    const extName = process.argv[2];
-    let takenFiles = await readDirectory();
-    if (takenFiles.length) {
-        let dirName = await makeDestDir();
-        let written = await createFiles(dirName, takenFiles, extName);
-    }
-
-    fs.readdir(__dirname, { withFileTypes: true }, (err, files) => {
-        if (err) {
-            console.log('err');
-        }
-        console.log('files', typeof files);
-        // for (const file of files) {
-            
-        // }
-
-    })
-}
-
-loopDirectory();
-
+readSyncSubDir(__dirname);
