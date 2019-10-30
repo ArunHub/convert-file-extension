f66dd1253affdcb644f2430513516a5f1b4afef4
Arun <arunsubmail@gmail.com>
2019-10-30 21:12:48 +0530

    deleted functional code n added stream functionality n added package.json

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ class ConvertFileExt {
             await this.createFiles(this.level === 0 ? this.destPath : createdDir, dirPath, fileList).then((res) => {
                 console.log('Done!!!');
             });
+            // await this.createStreamFiles(this.level === 0 ? this.destPath : createdDir, dirPath, fileList).then((res) => {
+            //     console.log('Done!!!');
+            // });
         }
         if (directories.length) {
             this.level++;
@@ -61,6 +64,14 @@ class ConvertFileExt {
         }
     }
 
+    // async createStreamFiles(dirName, readPath, fileList) {
+    //     for (const file of fileList) {
+    //         let fileExt = this.changeExtension(file);
+    //         const newPath = path.join(readPath, file);
+    //         this.writerStream(`${dirName}/${fileExt}`, newPath);
+    //     }
+    // }
+
     readFiles(readPath, regexFile) {
         const newPath = path.join(readPath, regexFile);
         return new Promise((resolve, reject) => {
@@ -71,6 +82,31 @@ class ConvertFileExt {
                 resolve(String(data));
             })
         })
+    }
+
+    readerStream(filename) {
+        const reader = fs.createReadStream(filename);
+        let chunks = [];
+
+        reader.on('data', (chunk) => { chunks.push(chunk) });
+
+        reader.on('close', () => chunks.push(null));
+
+        reader.on('error', (err) => { return cb(err) });
+
+        return reader;
+    }
+
+    writerStream(writeToFile, readFile) {
+        const writer = fs.createWriteStream(writeToFile);
+
+        let readable = this.readerStream(readFile);
+
+        readable.pipe(writer);
+
+        writer.on('data', () => { console.log('data written') });
+        writer.on('error', (err) => { throw err });
+        writer.on('close', () => console.log('writing done!!'));
     }
 
     getRandomPath(dirPath) {
@@ -91,87 +127,5 @@ class ConvertFileExt {
     }
 }
 
-// const readSyncSubDir = async (dirPath, extName, folderList = [], level = 0, destPath = "") => {
-//     const arrayList = fs.readdirSync(dirPath);
-//     let directories = [], fileList = [], createdDir = "";
-//     arrayList.forEach(file => {
-//         if (isDirectory(dirPath, file))
-//             directories = [...directories, file];
-//         else
-//             fileList = [...fileList, file];
-//     })
-//     if (fileList.length) {
-//         if (level === 0) {
-//             destPath = await makeDestDir(dirPath, level);
-//         } else {
-//             let lastWord = dirPath.split(path.sep);
-//             lastWord = lastWord[lastWord.length - 1];
-//             createdDir = await makeDestDir(destPath + '/' + lastWord, level);
-//         }
-//         await createFiles(level === 0 ? destPath : createdDir, dirPath, fileList, extName).then((res) => {
-//             console.log('Done!!!');
-//         });
-//     }
-//     if (directories.length) {
-//         level++;
-//         directories.forEach(folder => {
-//             folderList = [...folderList, path.join(dirPath, folder)];
-//             readSyncSubDir(path.join(dirPath, folder), extName, folderList, level, destPath);
-//         });
-//     }
-// }
-
-// const isDirectory = (dirName, file) => fs.statSync(path.join(dirName, file)).isDirectory();
-
-// const changeExtension = (file, extName) => {
-//     let ext = extName ? '.' + extName : '.js';
-//     return path.extname(file) === ".js" ? file.replace(/\.js$/g, ext) : file;
-// }
-
-// const createFiles = async (dirName, readPath, fileList, extName) => {
-//     for (const file of fileList) {
-//         let reading = await readFiles(readPath, file);
-//         let fileExt = changeExtension(file, extName);
-//         fs.writeFile(`${dirName}/${fileExt}`, reading, (err) => {
-//             if (err) {
-//                 console.log('error writing data=', err);
-//             }
-//             console.log('completed')
-//         })
-//     }
-// }
-
-// const readFiles = (readPath, regexFile) => {
-//     const newPath = path.join(readPath, regexFile);
-//     return new Promise((resolve, reject) => {
-//         fs.readFile(newPath, (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             }
-//             resolve(String(data));
-//         })
-//     })
-// }
-
-// const getRandomPath = (dirPath) => {
-//     let randomDir = dirPath.slice() + (Math.random() * (10 - 1) + 1).toFixed(2);
-//     return randomDir;
-// }
-
-// const makeDestDir = (dirPath, level) => {
-//     return new Promise((resolve, reject) => {
-//         const randomDir = level ? dirPath : getRandomPath(dirPath);
-//         fs.mkdir(randomDir, (err) => {
-//             if (err) {
-//                 reject('create directory failed because ' + err);
-//             }
-//             resolve(randomDir);
-//         })
-//     })
-// }
-
 const convertFile = new ConvertFileExt(process.argv[3]);
 convertFile.readSyncSubDir(process.argv[2]).then(res => console.log('All Files are converted'));
-// (async function start() {
-//     readSyncSubDir(process.argv[2], process.argv[3]);
-// })();
